d019b76c75ba5be8e4aa3c490209a2b695ee953f
na.liu <na.n.liu@oracle.com>
2019-07-25 18:40:00 +0800

    add flask-moment extention to support date, time and locale

--- hello.py
+++ hello.py
@@ -17,13 +17,16 @@ __VERSION__ = "1.0.0.07222019"
 # imports
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+from datetime import datetime
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', current_time=datetime.utcnow())
 
 @app.route('/user/<name>')
 def user(name):
