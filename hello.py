795616665989751f0ef8c68636d8e42bed9b9106
na.liu <na.n.liu@oracle.com>
2019-07-26 21:47:51 +0800

    use SQLAlchemy to communicate with db sqlite

--- hello.py
+++ hello.py
@@ -22,13 +22,21 @@ from datetime import datetime
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
-moment = Moment(app)
+basedir = os.path.abspath(os.path.dirname(__file__))
 
 # configuration
 app.config['SECRET_KEY'] = 'hard to guess string placeholder'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///{}'.format(os.path.join(basedir, 'data.sqlite'))
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+
+bootstrap = Bootstrap(app)
+moment = Moment(app)
+db = SQLAlchemy(app)
 
 
 # consts
@@ -65,6 +73,23 @@ class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+class Role(db.Model):
+    __tablename__ = 'roles'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), unique=True)
+    users = db.relationship('User', backref='role')
+
+    def __repr__(self):
+        return '<Role {}>'.format(self.name)
+
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(64), unique=True, index=True)
+    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
 
 class TODO(object):
     def __init__(self):
