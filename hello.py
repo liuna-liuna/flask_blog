fa658baab103039473ed9a9cd277e604572d1236
na.liu <na.n.liu@oracle.com>
2019-07-26 17:00:29 +0800

    use view function to process web forms

--- hello.py
+++ hello.py
@@ -15,18 +15,34 @@ __VERSION__ = "1.0.0.07222019"
 
 
 # imports
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect, url_for
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from datetime import datetime
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
-@app.route('/')
+# configuration
+app.config['SECRET_KEY'] = 'hard to guess string placeholder'
+
+
+# consts
+
+# functions
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
+    name, form = None, NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+        return redirect(url_for('.index'))
+    else:
+        return render_template('index.html', current_time=datetime.utcnow(), form=form, name=name)
 
 @app.route('/user/<name>')
 def user(name):
@@ -42,13 +58,12 @@ def internal_server_error(e):
     return render_template('500.html'), 500
 
 
-# configuration
-
-# consts
-
-# functions
-
 # classes
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
 class TODO(object):
     def __init__(self):
         pass
