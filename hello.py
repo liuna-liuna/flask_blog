cf6f393cf91898bf068f695a3d3ed65277dd086b
na.liu <na.n.liu@oracle.com>
2019-07-27 09:58:22 +0800

    integrate python shell via @app.shell_context_processor

--- hello.py
+++ hello.py
@@ -74,6 +74,10 @@ def page_not_found(e):
 def internal_server_error(e):
     return render_template('500.html'), 500
 
+# to integrate Python shell
+@app.shell_context_processor
+def make_shell_context():
+    return dict(db=db, User=User, Role=Role)
 
 # classes
 class NameForm(FlaskForm):
