af767feba5e6c7d08c79fb82c89f9dad47f4898d
na.liu <na.n.liu@oracle.com>
2019-07-30 11:31:06 +0800

    add codes for new user registration.

--- app/auth/views.py
+++ app/auth/views.py
@@ -18,8 +18,9 @@ __VERSION__ = "1.0.0.07292019"
 from flask import render_template, request, url_for, redirect, flash
 from flask_login import login_user, login_required, logout_user
 from . import auth
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 from ..models import User
+from .. import db
 
 # configuration
 
@@ -46,6 +47,20 @@ def logout():
     logout_user()
     flash('You have been logged out.')
     return redirect(url_for('main.index'))
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        user = User(email=form.email.data,
+                    username=form.username.data,
+                    password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('You can login now.')
+        return redirect(url_for('auth.login'))
+
+    return render_template('auth/register.html', form=form)
 
 # classes
 
