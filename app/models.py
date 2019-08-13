605078eb967a3517875b9d6dad28abb6eea0efc6
na.liu <na.n.liu@oracle.com>
2019-08-13 22:04:40 +0800

    use markdown format in post-processing on server side supporting rich text.

--- app/models.py
+++ app/models.py
@@ -21,6 +21,8 @@ from flask_login import UserMixin, AnonymousUserMixin
 from flask import current_app, request
 from datetime import datetime
 import hashlib
+from markdown import markdown
+import bleach
 from . import db, login_manager
 
 
@@ -222,14 +224,24 @@ class AnonymousUser(AnonymousUserMixin):
     def is_administrator(self):
         return False
 
-# configurations : set login_manager.anonymous_user
-login_manager.anonymous_user = AnonymousUser
 
 class Post(db.Model):
     __tablename__ = 'posts'
     id = db.Column(db.Integer, primary_key=True)
     body = db.Column(db.Text)
+    body_html = db.Column(db.Text)
     timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     author_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+
+    @staticmethod
+    def on_changed_body(target, value, oldvalue, initiator):
+        allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+                        'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul', 'h1', 'h2', 'h3', 'p']
+        target.body_html = bleach.linkify(bleach.clean(markdown(value, output_format='html'),
+                                                       tags=allowed_tags, strip=True))
+
+# configurations : set login_manager.anonymous_user, set db.event
+login_manager.anonymous_user = AnonymousUser
+db.event.listen(Post.body, 'set', Post.on_changed_body)
 
 # main entry
