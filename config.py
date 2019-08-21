76aab54a3e4792cf132cfec8acad69a354b9444c
na.liu <na.n.liu@oracle.com>
2019-08-21 16:35:52 +0800

    add SMTPHandler to logging for ProductionConfig

--- config.py
+++ config.py
@@ -58,6 +58,28 @@ class TestingConfig(Config):
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
                                              'sqlite:///{}'.format(os.path.join(basedir, 'data.sqlite'))
+    @classmethod
+    def init_app(cls, app):
+        Config.init_app(app)
+
+        # send email to administrator when error happens
+        import logging
+        from logging.handlers import SMTPHandler
+        credentials, secure = None, None
+        if getattr(cls, 'MAIL_USERNAME', None) is not None:
+            credentials = (cls.MAIL_USERNAME, cls.MAIL_PASSWORD)
+            if getattr(cls, 'MAIL_USE_TLS', None):
+                secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(cls.MAIL_SERVER, cls.MAIL_PORT),
+            fromaddr=cls.NA_BLOG_MAIL_SENDER,
+            toaddrs=[cls.NA_BLOG_ADMIN],
+            subject='{} Application Error'.format(cls.NA_BLOG_SUBJECT_PREFIX),
+            credentials=credentials,
+            secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        app.logger.addHandler(mail_handler)
+
 
 config = {'development': DevelopmentConfig,
           'testing': TestingConfig,
